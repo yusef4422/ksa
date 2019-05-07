8f47366c6a7b87971f5c14a4bfba457ed9e2aefd
yusef4422 <50304176+yusef4422@users.noreply.github.com>
2019-05-07 16:30:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
 const fs = require('file-system');
-const prefix = '#'
+const prefix = '-'
 var antispam = require("anti-spam");//npm i anti-spam
 
 
@@ -33,7 +33,7 @@ client.on('message', message => {
   }
 
 
-  if (message.content.startsWith('#bc')) {
+  if (message.content.startsWith('-bc')) {
           if (!args[0]) {
 message.channel.send("**#bc <message>**");
 return;
@@ -133,7 +133,7 @@ gg.send({embed : new Discord.RichEmbed()
 
 client.on('message', msg => {
 
-  if(msg.content.startsWith('#suggest')) {
+  if(msg.content.startsWith('-suggest')) {
     if(!msg.channel.guild) return msg.reply('** هاذا الامر فقط للسيرفرات**');
     if(!msg.guild.channels.find('name', 'suggestions')) return msg.reply('**الرجاء إضافة روم بإسم (suggestions)**');
     let args = msg.content.split(" ").slice(1);
@@ -231,16 +231,6 @@ moment.locale('ar-TN');
 
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`BOT STARTED`);
-        console.log(`---------------`);
-      console.log(`ON ${client.guilds.size} Servers `);
-    console.log(`---------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`Welcome To Vault Shop`,"http://twitch.tv/vAmmvr_");
-   client.user.setStatus("online");
-});
 
 
 client.on('message', message => {
@@ -510,5 +500,38 @@ client.on('message',function(message) {
        }
 });
 
+const developers = ["529336434939920395"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 
 client.login(process.env.BOT_TOKEN);
